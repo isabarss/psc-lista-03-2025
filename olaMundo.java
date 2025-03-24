f4725d4cc1a4c69a7c07537599a24254e3bee14e
Isabella Barros <barrosisabella1919@gmail.com>
2025-03-24 16:56:27 -0300

    olaMundo.java

---
+++ olaMundo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; 
+
+public class olaMundo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Olá mundo");
+    }
+}
